15e17d5f005e3066df7413a2d1610d8166d54784
sean stephen brian <seanstephenbrian@gmail.com>
2023-01-16 18:31:13 -0500

    create lower 'project' div in work => web view

--- src/js/web.js
+++ src/js/web.js
@@ -1,6 +1,9 @@
 import webHtml from '../html/web.html';
 
 function renderWeb() {
+
+    // STYLE 'web' LINK TO BE BOLD, OTHER WORK LINKS FADED
+
     const lowerSection = document.querySelector('.lower-section');
     lowerSection.innerHTML = webHtml;
 }
