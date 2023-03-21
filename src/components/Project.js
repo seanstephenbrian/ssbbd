72aa2ee0a6cfc56c7b5e91adb10f8114a6c04a70
sean stephen brian <seanstephenbrian@gmail.com>
2023-03-21 14:24:52 -0400

    dynamically render projects, populate w json data

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-export default function Project() {
+export default function Project(props) {
+    // receive project info as props from Projects component:
+    const { projectInfo } = props;
+
     return (
         <div className='project-card'>
-            project card
+            <div className='title'>
+                {projectInfo.title}
+            </div>
+            <div className='description'>
+                {projectInfo.description}
+            </div>
+            <a href={projectInfo.liveUrl}>
+                live link
+            </a>
+            <a href={projectInfo.gitUrl}>
+                git link
+            </a>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../styles/projects.scss';
 
 import Project from './Project';
 
 export default function Projects() {
+
+    // store project data in state:
+    const [projectData, setProjectData] = useState([]);
+
+    // fetch project info from local json file on component mount:
+    useEffect(() => {
+        fetch('./projects.json')
+        .then(res => res.json())
+        .then((projectData) => {
+            setProjectData(projectData.projects);
+        });
+    }, []);
+
+    useEffect(() => {
+        console.log(projectData);
+    }, [projectData])
+
     return (
         <section className='projects' id='projects'>
-            <Project />
-            <Project />
-            <Project />
-            <Project />
+            {projectData.map((project) => {
+                return (
+                    <Project projectInfo={project} />
+                )
+            })}
         </section>
     )
 }
