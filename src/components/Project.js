d6f211759b060010f11986f4ff273aa62f33c545
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-03-27 11:31:10 -0400

    style stack tags in project cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+
+import LinkIcon from '../img/icons/go-to-link.svg';
 
 export default function Project(props) {
     // receive project info as props from Projects component:
@@ -36,18 +38,25 @@ export default function Project(props) {
                 {
                     projectInfo.liveUrl ?
                     <a className='project-link live-link' href={projectInfo.liveUrl} target='_blank' rel='noopener noreferrer'>
-                        LIVE DEMO
+                        <span>LIVE DEMO</span>
+                        <img className='link-icon' src={LinkIcon} alt=''></img>
                     </a> :
                     ''
                 }
                 <a className='project-link gh-link' href={projectInfo.gitUrl} target='_blank' rel='noopener noreferrer'>
-                    GITHUB REPO
+                    <span>GITHUB REPO</span>
+                    <img className='link-icon' src={LinkIcon} alt=''></img>
                 </a>
             </div>
             <div className='project-stack'>
                 {projectInfo.stack.map((item) => {
                     return (
-                        <div className='stack-item'>
+                        <div 
+                            className={`stack-item ${item}-tag`}
+                            style={{
+                                backgroundColor: `var(--${item}-tag-bg)`
+                            }}
+                        >
                             {item}
                         </div>
                     )
