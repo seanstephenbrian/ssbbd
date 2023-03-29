fb4e73682e3827c7d510e0c58508117e1adb4710
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-03-29 14:10:42 -0400

    hide form after successful submission to emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 import emailjs from '@emailjs/browser';
 
@@ -6,36 +6,49 @@ import '../styles/contact-form.scss';
 
 export default function Contact() {
 
+    // track form status in state:
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
     const form = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
 
         emailjs.sendForm('service_q2lhk3c', 'template_fwlnf07', form.current, 'mSAIZ6QpOOWnDNHJc')
-            .then((result) => {
-                console.log(result.text);
+            .then(() => {
+                setFormSubmitted(true);
             }, (error) => {
                 console.log(error.text);
             });
     }
-    
-    return (
-        <section className='contact-form'>
-            <form ref={form} onSubmit={sendEmail}>
-                <section className='form-section'>
-                    <label htmlFor='from_name'>your name:</label>
-                    <input type='text' name='from_name' required></input>
-                </section>
-                <section className='form-section'>
-                    <label htmlFor='from_email'>your email:</label>
-                    <input type='email' name='from_email'></input>
-                </section>
-                <section className='form-section'>
-                    <label htmlFor='message'>your message:</label>
-                    <textarea name='message'></textarea>
-                </section>
-                <input type='submit'></input>
-            </form>
-        </section>
-    )
+
+    if (!formSubmitted) {
+        return (
+            <section className='contact-form'>
+                <form ref={form} onSubmit={sendEmail}>
+                    <section className='form-section'>
+                        <label htmlFor='from_name'>your name:</label>
+                        <input type='text' name='from_name' required></input>
+                    </section>
+                    <section className='form-section'>
+                        <label htmlFor='from_email'>your email:</label>
+                        <input type='email' name='from_email'></input>
+                    </section>
+                    <section className='form-section'>
+                        <label htmlFor='message'>your message:</label>
+                        <textarea name='message'></textarea>
+                    </section>
+                    <input type='submit'></input>
+                </form>
+            </section>
+        )
+    }
+
+    if (formSubmitted) {
+        return (
+            <section className='contact-form submission-confirmation'>
+                thanks for reaching out!
+            </section>
+        )
+    }
 }
